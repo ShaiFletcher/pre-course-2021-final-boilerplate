2d8c4b30c003817d38df78ec957cc6f9c666cef5
ShaiFletcher <shaixperiaz1@gmail.com>
2021-01-29 11:47:11 +0200

    create localstorage
    make localstorage work and get it to be printed in page when being refreshed or closed.

--- src/main.js
+++ src/main.js
@@ -4,61 +4,90 @@
  const prioritySelector = document.getElementById('priority-selector');
 
 let todos = [];
-// try {
-//   todos = localStorage.getItem("todos");
-// } catch (error) {
+try {
+  todos = JSON.parse(window.localStorage.getItem("todos"));
+  console.log(todos);
   
-// }
+  //if todos is not empty, make a loop that adds the todos to the ul
+  if (todos.length > 0) {
+    let initialView = document.getElementById("view");
+    //loop for each todo in todos
+    for (todo of todos){
+      let todoContainer = document.createElement("div");
+      todoContainer.className = "todo-container";
+      let todoPriority = document.createElement("div");
+      todoPriority.className ="todo-priority";
+      todoPriority.innerHTML = "Priority: " + todo.priority;
+      let todoCreatedAt = document.createElement("div");
+      todoCreatedAt.className = "todo-created-at";
+      todoCreatedAt.innerHTML = formatDate(todo.date);
+      let todoText = document.createElement("div");
+      todoText.className = "todo-text";
+      todoText.innerHTML = todo.text;
+      todoContainer.append(todoPriority, todoText, todoCreatedAt);
+      
+      initialView.append(todoContainer);
+    }
+    
+
+  }
+  //todoPriority.innerHTML = "Priority: " + todo.priority;
+
+} catch (error) {
+  
+}
 //event listeners
 addButton.addEventListener('click', addTodo);
+
+////////////////finished loading page
 
 // function
 function addTodo(event){
 
-//prevent form from submitting
-event.preventDefault();
+  //prevent form from submitting
+  event.preventDefault();
 
-//get task value
-let text = textInput.value;
-let priority = prioritySelector.value;
-let taskDate = new Date();
-console.log(text);
-//save todo as a JSON object in todos[]
-todos.push({ 
-  "text": text,
-  "priority": priority,
-  "date": taskDate
-});
-//save todos on local storage 
-localStorage.setItem("todos", todos);
+  //get task value
+  let text = textInput.value;
+  let priority = prioritySelector.value;
+  let taskDate = new Date();
 
-//save todos to JSONBIN
+  //save todo as a JSON object in todos[]
+  todos.push({ 
+    "text": text,
+    "priority": priority,
+    "date": taskDate
+  });
+  //save todos on local storage
+  window.localStorage.setItem("todos", JSON.stringify(todos));
 
-//create html elements, add to view section
-let todoContainer = document.createElement("div");
-todoContainer.className = "todo-container";
-let todoPriority = document.createElement("div");
-todoPriority.className ="todo-priority";
-todoPriority.innerHTML = "Priority: " + priority;
-let todoCreatedAt = document.createElement("div");
-todoCreatedAt.className = "todo-created-at";
-todoCreatedAt.innerHTML = formatDate(taskDate);
-let todoText = document.createElement("div");
-todoText.className = "todo-text";
-todoText.innerHTML = text;
-todoContainer.append(todoPriority, todoCreatedAt, todoText);
-let view = document.getElementById("view");
-view.append(todoContainer);
+  //save todos to JSONBIN
 
-//todo DIV
-const todoDIV = document.createElement('div');
-todoDIV.classList.add('todo');
-// create LI
-const newTodo = document.createElement('li');
-newTodo.classList.add('todo');
+  //create html elements, add to view section
+  let todoContainer = document.createElement("div");
+  todoContainer.className = "todo-container";
+  let todoPriority = document.createElement("div");
+  todoPriority.className ="todo-priority";
+  todoPriority.innerHTML = "Priority: " + priority;
+  let todoCreatedAt = document.createElement("div");
+  todoCreatedAt.className = "todo-created-at";
+  todoCreatedAt.innerHTML = formatDate(taskDate);
+  let todoText = document.createElement("div");
+  todoText.className = "todo-text";
+  todoText.innerHTML = text;
+  todoContainer.append(todoPriority, todoText, todoCreatedAt);
+  let view = document.getElementById("view");
+  view.append(todoContainer);
 
-//reset input
-textInput.value = "";
+  //todo DIV
+  const todoDIV = document.createElement('div');
+  todoDIV.classList.add('todo');
+  // create LI
+  const newTodo = document.createElement('li');
+  newTodo.classList.add('todo');
+
+  //reset input
+  textInput.value = "";
 
 }
 
